9fc6d6efafdc20a924318610802662952379f99a
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-01 09:17:01 -0700

    created constructors for recipe model

--- src/main/java/com/plateplanner/api/model/Recipe.java
+++ src/main/java/com/plateplanner/api/model/Recipe.java
@@ -7,4 +7,16 @@ public class Recipe {
     private Integer prepTime;
     private Integer cookTime;
     private String instructions;
+
+    public Recipe() {
+    }
+
+    public Recipe(Long id, String title, String description, Integer prepTime, Integer cookTime, String instructions) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.prepTime = prepTime;
+        this.cookTime = cookTime;
+        this.instructions = instructions;
+    }
 }
