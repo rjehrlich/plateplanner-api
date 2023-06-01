6180297a6f4354d433bd9b5e0dd4583946148b46
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-01 09:27:43 -0700

    created constructors, getters, setters for recipeingredient

--- src/main/java/com/plateplanner/api/model/RecipeIngredient.java
+++ src/main/java/com/plateplanner/api/model/RecipeIngredient.java
@@ -4,4 +4,46 @@ public class RecipeIngredient {
     private Recipe recipe;
     private Ingredient ingredient;
     private Integer quantity;
+
+    public RecipeIngredient() {
+    }
+
+    public RecipeIngredient(Recipe recipe, Ingredient ingredient, Integer quantity) {
+        this.recipe = recipe;
+        this.ingredient = ingredient;
+        this.quantity = quantity;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeIngredient{" +
+                "recipe=" + recipe +
+                ", ingredient=" + ingredient +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
