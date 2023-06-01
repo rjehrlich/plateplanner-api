fb23e6ce660a85ee1703d20994a2aec474a4bbea
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-01 09:22:38 -0700

    created constructors & getter setters for ingredient model

--- src/main/java/com/plateplanner/api/model/Ingredient.java
+++ src/main/java/com/plateplanner/api/model/Ingredient.java
@@ -3,4 +3,36 @@ package com.plateplanner.api.model;
 public class Ingredient {
     private Long id;
     private String name;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
