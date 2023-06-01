205079d2e8361b93bd38cffaa829d9c33ae0c414
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-01 16:11:50 -0700

    added recipeingredient repo back in

---
+++ src/main/java/com/plateplanner/api/repository/RecipeIngredientRepo.java
@@ -0,0 +1,6 @@
+package com.plateplanner.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecipeIngredientRepo extends JpaRepository<RecipeIngredientRepo, Long> {
+}
