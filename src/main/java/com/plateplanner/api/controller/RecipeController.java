6dfab480aeb64973bd866b3436dd04e7927ea4e0
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-01 15:44:46 -0700

    added get mapping for recipe controller

--- src/main/java/com/plateplanner/api/controller/RecipeController.java
+++ src/main/java/com/plateplanner/api/controller/RecipeController.java
@@ -1,4 +1,27 @@
 package com.plateplanner.api.controller;
 
+import com.plateplanner.api.model.Recipe;
+import com.plateplanner.api.repository.RecipeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/recipe")
 public class RecipeController {
+
+    private RecipeRepo recipeRepo;
+
+    @Autowired
+    public void setRecipeRepo(RecipeRepo recipeRepo) {
+        this.recipeRepo = recipeRepo;
+    }
+
+    @GetMapping(path = "/all")
+    public List<Recipe> getRecipes() {
+        return recipeRepo.findAll();
+    }
 }
