0616eff12c9dfc7bcfc44a98f14efb246d6083cd
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-01 09:19:25 -0700

    created getters and setters for recipe model

--- src/main/java/com/plateplanner/api/model/Recipe.java
+++ src/main/java/com/plateplanner/api/model/Recipe.java
@@ -19,4 +19,64 @@ public class Recipe {
         this.cookTime = cookTime;
         this.instructions = instructions;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public Integer getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", prepTime=" + prepTime +
+                ", cookTime=" + cookTime +
+                ", instructions='" + instructions + '\'' +
+                '}';
+    }
 }
