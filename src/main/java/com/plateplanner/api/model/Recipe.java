28bd89d0c2e4b6f5abbb1f9a8c082bf06b6ca501
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-01 09:13:46 -0700

    added recipe model

---
+++ src/main/java/com/plateplanner/api/model/Recipe.java
@@ -0,0 +1,10 @@
+package com.plateplanner.api.model;
+
+public class Recipe {
+    private Long id;
+    private String title;
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private String instructions;
+}
