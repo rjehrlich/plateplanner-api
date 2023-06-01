bd7b8f93c4b8876cfc0713a4a14e91baf7090321
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-01 15:33:47 -0700

    setup Cucumber testing files

---
+++ src/test/java/definitions/SpringBootCucumberTestDefinitions.java
@@ -0,0 +1,9 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+public class SpringBootCucumberTestDefinitions {
+    @Given("A list of recipes are available")
+    public void aListOfRecipesAreAvailable() {
+    }
+}
---
+++ src/test/resources/features/SpringBootCucumberTestFeature.feature
@@ -0,0 +1,4 @@
+Feature: Rest API Functionalities
+
+  Scenario: User able to view all recipes
+    Given A list of recipes are available
