f08d0539f8deafc60c8a465f02815a328f4a9a93
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-01 15:52:43 -0700

    refactored recipecontroller and rerun cucumber test

--- src/main/java/com/plateplanner/api/repository/RecipeIngredientRepo.java
+++
@@ -1,7 +0,0 @@
-package com.plateplanner.api.repository;
-
-import com.plateplanner.api.model.RecipeIngredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeIngredientRepo extends JpaRepository<RecipeIngredient, Long> {
-}
--- src/test/java/definitions/SpringBootCucumberTestDefinitions.java
+++ src/test/java/definitions/SpringBootCucumberTestDefinitions.java
@@ -1,8 +1,20 @@
 package definitions;
 
+import com.plateplanner.api.ApiApplication;
 import io.cucumber.java.en.Given;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ApiApplication.class)
 public class SpringBootCucumberTestDefinitions {
+
+    private static final String BASE_URL = "http://localhost:";
+
+    @LocalServerPort
+    String port;
+
     @Given("A list of recipes are available")
     public void aListOfRecipesAreAvailable() {
     }
