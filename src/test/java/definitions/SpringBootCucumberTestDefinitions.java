44c22088d22dc466cfda0cc3cc9a7184184be073
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-02 07:31:11 -0700

    created test for list of recipes

--- src/test/java/definitions/SpringBootCucumberTestDefinitions.java
+++ src/test/java/definitions/SpringBootCucumberTestDefinitions.java
@@ -8,10 +8,20 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
 
 
 @CucumberContextConfiguration
@@ -28,9 +38,10 @@ public class SpringBootCucumberTestDefinitions {
     @Autowired
     private RecipeRepo recipeRepo;
 
-    @Before
-    public void setUpTestData() {
-        // Populate test data before each scenario
+
+
+    @Given("A list of recipes are available")
+    public void aListOfRecipesAreAvailable() {
         Recipe recipe1 = new Recipe();
         // Set properties for recipe1
         recipeRepo.save(recipe1);
@@ -38,12 +49,18 @@ public class SpringBootCucumberTestDefinitions {
         Recipe recipe2 = new Recipe();
         // Set properties for recipe2
         recipeRepo.save(recipe2);
-    }
 
-
-    @Given("A list of recipes are available")
-    public void aListOfRecipesAreAvailable() {
-
+        try {
+            ResponseEntity<String> response = new RestTemplate()
+                    .exchange(BASE_URL + port + "/api/recipes", HttpMethod.GET, null, String.class);
+            List<Map<String, String>> recipes = JsonPath
+                    .from(String.valueOf(response
+                            .getBody())).get();
+            Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+            Assert.assertTrue(recipes.size() > 0);
+        } catch (HttpClientErrorException e) {
+            e.printStackTrace();
+        }
     }
 
     @When("I search for a recipe by id")
