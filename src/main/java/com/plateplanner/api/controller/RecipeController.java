f33ece66e63b83a287fe2a1456dc81e891eb26eb
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-01 17:41:42 -0700

    created endpoint for recipe by id

--- src/main/java/com/plateplanner/api/controller/RecipeController.java
+++ src/main/java/com/plateplanner/api/controller/RecipeController.java
@@ -1,13 +1,16 @@
 package com.plateplanner.api.controller;
 
+import com.plateplanner.api.exception.InformationNotFoundException;
 import com.plateplanner.api.model.Recipe;
 import com.plateplanner.api.repository.RecipeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -21,11 +24,21 @@ public class RecipeController {
     }
 
     /**
-     * getRecipes method provides a recipes Endpoint that will:
+     * getRecipes method provides a get request Endpoint that will:
      * @return a list of all recipes
      */
     @GetMapping(path = "/recipes")
     public List<Recipe> getRecipes() {
         return recipeRepo.findAll();
+    }
+
+    /**
+     * getRecipe method provides a get request Endpoint that will take in:
+     * @param recipeId
+     * @return that recipe by ID
+     */
+    @GetMapping(path = "recipes/{recipeId}")
+    public Optional<Recipe> getRecipe(@PathVariable(value = "recipeId") Long recipeId) {
+        return recipeRepo.findById(recipeId);
     }
 }
--- src/test/resources/features/SpringBootCucumberTestFeature.feature
+++ src/test/resources/features/SpringBootCucumberTestFeature.feature
@@ -1,4 +1,4 @@
 Feature: Rest API Functionalities
 
-  Scenario: User able to view all recipes
+  Scenario: User able to view all Recipes
     Given A list of recipes are available
