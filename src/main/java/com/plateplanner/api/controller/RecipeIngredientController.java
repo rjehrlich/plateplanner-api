abdb791278d556a810877857434834a3a6e0284d
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-04 23:06:01 -0700

    fixed issue with recipeingredient controller

--- src/main/java/com/plateplanner/api/controller/RecipeIngredientController.java
+++ src/main/java/com/plateplanner/api/controller/RecipeIngredientController.java
@@ -1,36 +1,50 @@
 package com.plateplanner.api.controller;
 
 import com.plateplanner.api.exception.InformationNotFoundException;
+import com.plateplanner.api.model.Ingredient;
+import com.plateplanner.api.model.Recipe;
 import com.plateplanner.api.model.RecipeIngredient;
 import com.plateplanner.api.repository.RecipeIngredientRepo;
+import com.plateplanner.api.repository.RecipeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "/recipes/{recipeId}/ingredients")
 public class RecipeIngredientController {
 
     private RecipeIngredientRepo recipeIngredientRepo;
+
+    private RecipeRepo recipeRepo;
 
     @Autowired
     public void setRecipeIngredientRepo(RecipeIngredientRepo recipeIngredientRepo) {
         this.recipeIngredientRepo = recipeIngredientRepo;
     }
 
+    @Autowired
+    public void setRecipeRepo(RecipeRepo recipeRepo) {
+        this.recipeRepo = recipeRepo;
+    }
+
     /**
      * getRecipeIngredients method provides an endpoint that will take in:
+     *
      * @param recipeId and
      * @return the ingredients in a list of that recipe
      */
-    @GetMapping
-    public List<RecipeIngredient> getRecipeIngredients(@PathVariable Long recipeId) {
-        return recipeIngredientRepo.findAllByRecipeId(recipeId);
+    @GetMapping(path = "/recipes/{recipeId}/ingredients")
+    public List<Ingredient> getRecipeIngredients(@PathVariable Long recipeId) {
+        Optional<Recipe> recipe = recipeRepo.findById(recipeId);
+        if (recipe.isEmpty()) {
+            throw new InformationNotFoundException("Recipe with ID " + recipeId + " not found");
+        } else {
+            return recipe.get().getIngredients();
+        }
     }
 
     /**
