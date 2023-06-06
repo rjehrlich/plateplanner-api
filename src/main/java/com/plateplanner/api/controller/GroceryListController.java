d904ea7f7389776ceb61f24e0f58d98a9a2f2bf9
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-05 21:31:54 -0700

    grocerylist service layer

--- src/main/java/com/plateplanner/api/controller/GroceryListController.java
+++ src/main/java/com/plateplanner/api/controller/GroceryListController.java
@@ -1,32 +1,22 @@
 package com.plateplanner.api.controller;
 
 import com.plateplanner.api.model.GroceryList;
-import com.plateplanner.api.model.Ingredient;
-import com.plateplanner.api.model.Recipe;
-import com.plateplanner.api.repository.GroceryListRepo;
-import com.plateplanner.api.repository.RecipeRepo;
+import com.plateplanner.api.service.GroceryListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class GroceryListController {
 
-    private RecipeRepo recipeRepo;
-    private GroceryListRepo groceryListRepo;
+    private GroceryListService groceryListService;
 
-    /**
-     * GroceryListController constructor takes the dependencies:
-     * @param recipeRepo
-     * @param groceryListRepo
-     * and injects them into the class
-     */
-    public GroceryListController(RecipeRepo recipeRepo, GroceryListRepo groceryListRepo) {
-        this.recipeRepo = recipeRepo;
-        this.groceryListRepo = groceryListRepo;
+    @Autowired
+    public void setGroceryListService(GroceryListService groceryListService) {
+        this.groceryListService = groceryListService;
     }
 
     /**
@@ -36,21 +26,7 @@ public class GroceryListController {
      */
     @PostMapping("/grocery-lists")
     public GroceryList generateGroceryList(@RequestBody List<Long> recipeIds) {
-        // Retrieve the selected recipes from the recipe IDs
-        List<Recipe> recipes = recipeRepo.findAllById(recipeIds);
-
-        // Extract the ingredients from the selected recipes
-        List<Ingredient> ingredients = new ArrayList<>();
-        for (Recipe recipe : recipes) {
-            List<Ingredient> recipeIngredients = recipe.getIngredients();
-            ingredients.addAll(recipeIngredients);
-        }
-
-        // Create the grocery list with the aggregated ingredients
-        GroceryList groceryList = new GroceryList();
-        groceryList.setIngredients(ingredients);
-
-        // Save the grocery list to the database
-        return groceryListRepo.save(groceryList);
+        return groceryListService.generateGroceryList(recipeIds);
     }
+
 }
--- src/main/java/com/plateplanner/api/service/GroceryListService.java
+++ src/main/java/com/plateplanner/api/service/GroceryListService.java
@@ -1,4 +1,53 @@
 package com.plateplanner.api.service;
 
+import com.plateplanner.api.model.GroceryList;
+import com.plateplanner.api.model.Ingredient;
+import com.plateplanner.api.model.Recipe;
+import com.plateplanner.api.repository.GroceryListRepo;
+import com.plateplanner.api.repository.RecipeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class GroceryListService {
+
+    private RecipeRepo recipeRepo;
+    private GroceryListRepo groceryListRepo;
+
+    @Autowired
+    public void setRecipeRepo(RecipeRepo recipeRepo) {
+        this.recipeRepo = recipeRepo;
+    }
+
+    @Autowired
+    public void setGroceryListRepo(GroceryListRepo groceryListRepo) {
+        this.groceryListRepo = groceryListRepo;
+    }
+
+    /**
+     * generateGroceryList method takes in multiple:
+     * @param recipeIds and
+     * @return all ingredients in a list for ALL recipes
+     */
+    public GroceryList generateGroceryList(List<Long> recipeIds) {
+        // Retrieve the selected recipes from the recipe IDs
+        List<Recipe> recipes = recipeRepo.findAllById(recipeIds);
+
+        // Extract the ingredients from the selected recipes
+        List<Ingredient> ingredients = new ArrayList<>();
+        for (Recipe recipe : recipes) {
+            List<Ingredient> recipeIngredients = recipe.getIngredients();
+            ingredients.addAll(recipeIngredients);
+        }
+
+        // Create the grocery list with the aggregated ingredients
+        GroceryList groceryList = new GroceryList();
+        groceryList.setIngredients(ingredients);
+
+        // Save the grocery list to the database
+        return groceryListRepo.save(groceryList);
+    }
 }
